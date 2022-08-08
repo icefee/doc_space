ccc1994cd4415f9ef0cde8b8976687d23d0d49ee
ying_123 <zhangying@aten-optics.com>
2022-08-08 17:45:13 +0800

    update s..

--- src/app/articles/flutter/scaffold-message-material-banner/scaffold-message-material-banner.component.ts
+++ src/app/articles/flutter/scaffold-message-material-banner/scaffold-message-material-banner.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PageComponent } from 'src/app/page/page.component';
 
 @Component({
   selector: 'app-scaffold-message-material-banner',
   templateUrl: './scaffold-message-material-banner.component.html',
   styleUrls: ['./scaffold-message-material-banner.component.css']
 })
-export class ScaffoldMessageMaterialBannerComponent implements OnInit {
+export class ScaffoldMessageMaterialBannerComponent extends PageComponent implements OnInit {
 
-  constructor() { }
+  public page_dart = '';
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const [page_dart] = await this.loadCodes(
+      [
+        'flutter/scaffold-message-material-banner/page_dart.pre'
+      ]
+    );
+    this.page_dart = page_dart;
+    setTimeout(this.initHljs, 0);
   }
 
 }
